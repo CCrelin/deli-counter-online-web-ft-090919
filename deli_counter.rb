100e3641d033e729d2b8ace42d17e2d365c435fc
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-14 06:42:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,10 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
   katz_deli.each do |name|
-  message =  " #{number}.#{name}"
+  message =  " #{number}. #{name}"
   array << message
   number += 1 
   end
 
-puts "The line is currently: #{array.join(" ")}" 
+puts "The line is currently: #{array.join("")}" 
 end

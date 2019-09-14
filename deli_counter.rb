0e52c31b0669cf9f1e0a8e3fc93508338dd774d5
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-14 08:16:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,8 +23,7 @@ def now_serving(line)
   if line == []
     puts "There is nobody waiting to be served!"
   else 
-    line.drop 
-    puts "Currently serving Logan."
+    puts "Currently serving #{line.drop}."
   end
 end
 

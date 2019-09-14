d0b65cf8e7374f3252e859bcb206222aea210b83
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-14 07:43:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ end
 def take_a_number(line, new_person)
   if line == [] 
    line << new_person
-   puts "Welcome, #{new_person}. You are number 1 in line."
+   puts "Welcome, #{new_person}. You are number #{line.length} in line."
   end
 end
 

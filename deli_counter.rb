df3db39c1d90b9602229452413ee0363341b339f
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-14 05:12:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
-  elsif  katz_deli 
+  elsif line == other_deli
+  puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
     
   end
 end
